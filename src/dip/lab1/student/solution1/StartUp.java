75b7da6ac8a81110c950c724143588aca09a5070
Matthew_2 <Matthew_2@Matthew.attlocal.net>
2015-09-20 18:03:48 -0500

    3rd Commit - finished the first lab. got a error with java 1.7 or something. still run perfectly fine if you right click and select "run file" :) .

--- src/dip/lab1/student/solution1/StartUp.java
+++ src/dip/lab1/student/solution1/StartUp.java
@@ -13,12 +13,15 @@ public class StartUp {
     
     public static void main(String[] args) {
      
+        Employee EmpArray [] =  new Employee[]  {
+                                                new HourEmp(11.75,750),
+                                                new HourEmpPlusTip(11.00,800),
+                                                new SalaryEmployees(55000,500)}; 
+        HRService hrs = new HRService();
         
-        
-        
-        
-        
-        
+        for (Employee emp : EmpArray) {
+            System.out.println("Employees yearly compensation: $" + hrs.getYearlyEmpComp(emp));
+        }
         
         
         
