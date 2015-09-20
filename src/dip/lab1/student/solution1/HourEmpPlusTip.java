aacc8518f659a64a6147cd7dcd4835341ed65a78
Matthew_2 <Matthew_2@Matthew.attlocal.net>
2015-09-20 17:20:25 -0500

    2nd Commit - Added HourEmpPlus Tip. to show that program is flexible.  and validateion to the setter to prevent errors

--- src/dip/lab1/student/solution1/HourEmpPlusTip.java
+++ src/dip/lab1/student/solution1/HourEmpPlusTip.java
@@ -9,6 +9,49 @@ package dip.lab1.student.solution1;
  *
  * @author Matthew_2
  */
-public class HourEmpPlusTip {
-    
+public class HourEmpPlusTip implements Employee {
+        private double hourRate;
+        private double totHoursPerYr;
+
+    public HourEmpPlusTip(double hourRate, double totHoursPerYr) {
+        this.hourRate = hourRate;
+        this.totHoursPerYr = totHoursPerYr;
+    }
+        
+        @Override
+         public final double getYearlyEarnings() {
+        return hourRate * totHoursPerYr;
+    }
+    // final concrete methods. also don't want them to change
+
+    public final double getHourRate() {
+        return hourRate + 20; // to show that you can add more lower Modules
+                                          // w/o breaking the program
+    }
+
+    //validateing because you don't want negative number
+    //of $'s per hours or  to much of asurplus
+    public final void setHourRate(double hourRate) {
+        if (hourRate < 0 || hourRate > 400) {
+            System.out.println("Illegal $'s per hour");
+        }
+        this.hourRate = hourRate;
+    }
+
+    public final double getTotHoursPerYr() {
+        return totHoursPerYr;
+    }
+
+    public final void setTotHoursPerYr(double totHoursPerYr) {
+        if (totHoursPerYr <0 || totHoursPerYr > 4500){
+            System.out.println("Illegal Hour limit");
+        }
+        this.totHoursPerYr = totHoursPerYr;
+    }
+        
+        
+        
+        
+        
+        
 }
