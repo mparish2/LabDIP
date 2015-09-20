802e4e4328cc4479dbc34f3e76599c395c606c80
Matthew_2 <Matthew_2@Matthew.attlocal.net>
2015-09-20 16:50:25 -0500

    1st commit - thinking of adding a interface to get the Yearly wages because it is common between the subclasses and plus all will very and need to be implemented. got ride of other varibles in employee because doesn't make sense to have them their.

---
+++ src/dip/lab1/student/solution1/HourEmp.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dip.lab1.student.solution1;
+
+/**
+ *
+ * @author Matthew_2
+ */
+public class HourEmp {
+    private double hourRate;
+    private double totHoursPerYr;
+    
+    
+    
+    
+    
+}
---
+++ src/dip/lab1/student/solution1/StartUp.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dip.lab1.student.solution1;
+
+/**
+ *
+ * @author Matthew_2
+ */
+public class StartUp {
+    
+    public static void main(String[] args) {
+     
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
